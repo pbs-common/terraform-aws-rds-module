59bb902efc1ea8992a026016bcec8d786b4345bd
Dragos Andreescu <dragos.andreescu@3pillarglobal.com>
2025-10-23 15:16:07 +0300

    CAT-25805 - add sources for providers

--- terraform.tf
+++ terraform.tf
@@ -3,9 +3,11 @@ terraform {
   required_providers {
     # tflint-ignore: terraform_unused_required_providers
     aws = {
+      source  = "hashicorp/aws"
       version = ">= 6.0.0"
     }
     random = {
+      source  = "hashicorp/random"
       version = ">= 3.1.0"
     }
   }
