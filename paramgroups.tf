c8ee86495ddb64cb9947231c4b13013ebf075c07
Cristian Cioltan <cristian.cioltan@3pillarglobal.com>
2026-05-21 10:52:36 +0300

    CAT-26504 - Adding various input variables for the module in order to import terraform resources which didn't follow the naming convention of this module

--- paramgroups.tf
+++ paramgroups.tf
@@ -1,7 +1,7 @@
 resource "aws_rds_cluster_parameter_group" "this" {
-  name        = "${local.name}-${local.parameter_group_name_suffix}"
+  name        = var.db_cluster_parameter_group_resource_name != null ? var.db_cluster_parameter_group_resource_name : "${local.name}-${local.parameter_group_name_suffix}"
   family      = local.parameter_group_family
-  description = "RDS Cluster parameter group for ${local.name}"
+  description = var.db_cluster_parameter_group_description != null ? var.db_cluster_parameter_group_description : "RDS Cluster parameter group for ${local.name}"
 
   dynamic "parameter" {
     for_each = var.db_cluster_parameters
@@ -20,8 +20,8 @@ resource "aws_rds_cluster_parameter_group" "this" {
 }
 
 resource "aws_db_parameter_group" "this" {
-  name        = "${local.name}-${local.parameter_group_name_suffix}"
-  description = "RDS Instance parameter group for ${local.name}"
+  name        = var.db_instance_parameter_group_resource_name != null ? var.db_instance_parameter_group_resource_name : "${local.name}-${local.parameter_group_name_suffix}"
+  description = var.db_instance_parameter_group_description != null ? var.db_instance_parameter_group_description : "RDS Instance parameter group for ${local.name}"
   family      = local.parameter_group_family
 
   dynamic "parameter" {
